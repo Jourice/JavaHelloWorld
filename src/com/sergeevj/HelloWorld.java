a6db38bc057f4bd99f08deb841c2e376377e1877
Jourii Sergeev <sergeevjdev@gmail.com>
2016-04-07 17:00:41 +0500

    hello world!

---
+++ src/com/sergeevj/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sergeevj;
+
+/**
+ * Created by sergeev on 07.04.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
